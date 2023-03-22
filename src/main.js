574baab1321527ae87b4e893ddfae97e8abd5ef7
shira kantor <shira.shachar@gmail.com>
2023-03-22 17:41:01 +0200

    filter nav and carusel

--- src/main.js
+++ src/main.js
@@ -6,9 +6,12 @@ import { store } from './store/store.js'
 // import './animate.css';
 import './assets/styles/main.scss'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 app.use(router)
 app.use(store)
 
+app.use(ElementPlus)
 app.mount('#app')
