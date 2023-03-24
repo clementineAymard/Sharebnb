e2255278da1c05d539e23994b079643bcfdaec53
Clementine Amar <clementine_cohen@hotmail.com>
2023-03-24 16:57:33 +0300

    guests picker in  progress

--- src/cmps/AppHeader.vue
+++ src/cmps/AppHeader.vue
@@ -147,6 +147,9 @@ export default {
             this.selectedFilterKey = 'locations'
         }
     },
+    watch: {
+
+    },
     components: {
         BrandLogo,
         LocationPicker,
---
+++ src/cmps/GuestsPicker.vue
@@ -0,0 +1,49 @@
+<template>
+    <section class="guests-picker flex column">
+        <div class="adults flex align-center justify-between">
+            <div>
+                <div class="title">Adults</div>
+                <div class="sub-title">Ages 13 or above</div>
+            </div>
+            <div class="selector"></div>
+        </div>
+        <div class="children flex align-center justify-between">
+            <div>
+                <div class="title">Children</div>
+                <div class="sub-title">Ages 2-12</div>
+            </div>
+            <div class="selector"></div>
+        </div>
+        <div class="infants flex align-center justify-between">
+            <div>
+                <div class="title">Infants</div>
+                <div class="sub-title">Under 2</div>
+            </div>
+            <div class="selector"></div>
+        </div>
+    </section>
+</template>
+
+<script>
+
+export default {
+    name: '',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+    created() {
+
+    },
+    components: {
+
+    },
+}
+</script>
