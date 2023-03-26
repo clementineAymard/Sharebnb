52225d40f6849ba7eeb6d5b9e2a5f3470cd71f75
shira kantor <shira.shachar@gmail.com>
2023-03-26 12:15:44 +0300

    stayEdit

--- src/pages/StayEdit.vue
+++ src/pages/StayEdit.vue
@@ -1,16 +1,26 @@
 <template>
-    <section class="stay-edit">
-        // TODO : Create template same as stay-details but with a form : input texts, checkboxes and upload images
-        // all with v-model for data's stay
+    <section class="stay-edit flex column  align-center">
+        <div>
+            <div class="editTitle">Airbnb it.</div> 
+             <div class="editSecondLine">You could earn</div> 
+            <div class="money"> $1,337</div>
+           
+            <div class="estimated "><span class="nights">7 nights </span> at an estimated $191 a night</div> 
+        <el-slider :min="1" :max="30" value="7" class="nightsRange" id="nightsRange" v-model="value"/>
+       <div class="estimate">Learn how we estimate your earnings</div>
+    </div>
     </section>
 </template>
 
 <script>
+// TODO : Create template same as stay-details but with a form : input texts, checkboxes and upload images
+// all with v-model for data's stay
 
 // import stay service
 export default {
     data() {
         return {
+            value: 7
             // stay : null
         }
     },
