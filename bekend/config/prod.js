521eee0810f5264844424a2c00f32c31a1770430
Clementine Amar <clementine_cohen@hotmail.com>
2023-03-30 17:23:49 +0300

    moved repo to include backend

--- bekend/config/prod.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-   dvUrl : 'mongodb+srv://shira:1234@cluster0.qehjfs4.mongodb.net/?retryWrites=true&w=majority',
-dbName : 'sharebnb_db'
-} 
---
+++ frontend/src/cmps/DatePickerSmall.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="date-picker-small flex ">
+        <el-date-picker ref="calendar" v-model="dates" type="daterange" @change="setDates" />
+    </div>
+</template>
+
+<script>
+
+import { ref } from 'vue'
+
+export default {
+    name: '',
+    data() {
+        return {
+            dates: []
+        }
+    },
+    methods: {
+        setDates(dates) {
+            console.log('this.dates', this.dates)
+            this.$emit('setDates', { ...this.dates })
+        }
+    },
+    computed: {
+
+    },
+    created() {
+    },
+    components: {
+
+    },
+    unmounted() {
+        // this.$emit('setDates', this.dates)
+    }
+}
+</script>
